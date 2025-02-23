9f11d3bb11752f34f99553703dde29eacc94e250
Franco <franschuler13@gmail.com>
2025-02-23 10:52:09 +0100

    Added and applied prettier to project

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,19 +1,29 @@
-"use client"
+'use client'
 
-import { MouseEventHandler } from "react"
+import { MouseEventHandler } from 'react'
 
 interface Props {
-    title: string,
-    containerStyles?: string,
-    handleClick?: MouseEventHandler<HTMLButtonElement>
-    type?: "button" | "submit"
+	title: string
+	containerStyles?: string
+	handleClick?: MouseEventHandler<HTMLButtonElement>
+	type?: 'button' | 'submit'
 }
-const Button = ({title, containerStyles, handleClick, type = "button"}: Props) => {
-  return (
-    <button disabled={false} type={"button"} className={`custom-btn ${containerStyles}`} onClick={handleClick}>
-        <span className={`flex-1`}>{title}</span>
-    </button>
-  )
+const Button = ({
+	title,
+	containerStyles,
+	handleClick,
+	type = 'button',
+}: Props) => {
+	return (
+		<button
+			disabled={false}
+			type={'button'}
+			className={`custom-btn ${containerStyles}`}
+			onClick={handleClick}
+		>
+			<span className={`flex-1`}>{title}</span>
+		</button>
+	)
 }
 
 export default Button
--- components/index.ts
+++ components/index.ts
@@ -2,4 +2,4 @@ import Hero from './Hero'
 import Button from './Button'
 import Navbar from './Navbar'
 import Footer from './Footer'
-export {Hero, Button, Navbar, Footer}
+export { Hero, Button, Navbar, Footer }
